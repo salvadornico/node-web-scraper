58c4e0706c8efcf5134f4a114bb678091484ee0d
Nico Salvador <nicosalvador@creatinginfo.com>
2018-06-20 17:34:46 +0800

    First commit

---
+++ scrape.js
@@ -0,0 +1,29 @@
+const puppeteer = require("puppeteer")
+
+let scrape = async () => {
+	const browser = await puppeteer.launch({
+		headless: false
+	})
+	const page = await browser.newPage()
+
+	await page.goto("http://books.toscrape.com/")
+	await page.waitFor(1000)
+
+	await page.click("li.col-xs-6:nth-child(1) > article:nth-child(1) > h3:nth-child(3) > a:nth-child(1)")
+	const result = await page.evaluate(() => {
+		let title = document.querySelector("h1").innerText
+		let price = document.querySelector(".price_color").innerText
+
+		return {
+			title,
+			price
+		}
+	})
+
+	browser.close()
+	return result
+}
+
+scrape().then(value => {
+	console.log(value);
+})
